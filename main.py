e1c54b707390a095f952f33f95c39149c1b4020e
mrstrangerpvt2024-cmyk <mrstrangerpvt2024@gmail.com>
2025-11-16 17:05:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -893,8 +893,7 @@ async def txt_handler(bot: Client, m: Message):
                     except FloodWait as e:
                         await m.reply_text(str(e))
                         time.sleep(e.x)
-                        continue    
-  
+                
                 elif ".pdf" in url:
                     if "cwmediabkt99" in url:
                         max_retries = 3  # Define the maximum number of retries
@@ -1416,6 +1415,7 @@ if __name__ == "__main__":
     notify_owner() 
 
 bot.run()
+
 
 
 
